1c4c92ddfeaad501b9e807256ff46403fd362fcb
tholivera <emaildathaisoliveira.silva@gmail.com>
2023-02-07 20:46:21 -0300

    Atualizando validações

--- aula-02/aula02.js
+++ aula-02/aula02.js
@@ -1,3 +1,5 @@
+alert("Oi! Vamos somar?")
+
 let primeiroNum = parseInt(prompt("Digite um número"))
 let segundoNum = parseInt(prompt("Digite um número diferente do anterior"))
 
@@ -8,22 +10,29 @@ if (primeiroNum == segundoNum) {
     }
     
 } 
+
 let soma = primeiroNum + segundoNum
 
 if (primeiroNum < 10) {
     alert("O primeiro número digitado é menor que 10")
-} else if (primeiroNum == 10 || primeiroNum <= 50 ) {
+} else if (primeiroNum >= 10 && primeiroNum <= 50 ) {
     alert("O primeiro número digitado está entre 10 e 50")
-} else {
+} else if (primeiroNum > 50 ){
     alert("O primeiro número digitado é maior que 50")
+} else {
+    alert("Você não digitou o primeiro número, por isso não vou conseguir somar :(")
 }
 
 if (segundoNum < 10) {
     alert("O segundo número digitado é menor que 10")
-} else if (segundoNum == 10 || segundoNum <= 50 ) {
+} else if (segundoNum >= 10 && segundoNum <= 50 ) {
     alert("O segundo número digitado está entre 10 e 50")
-} else {
-    alert("O segundo número digitado é maior que 50")
+} else if (segundoNum > 50 ){
+    alert("O primeiro número digitado é maior que 50")
+}else {
+    alert("Você não digitou o segundo número, por isso não vou conseguir somar :(")
 }
 
-alert(`A soma de ${primeiroNum + " + " + segundoNum} é igual a ${soma}\nAgradeço a colaboração`)
+if (soma >= 0) {
+    alert(`A soma de ${primeiroNum + " + " + segundoNum} é igual a ${soma}\nAgradeço a colaboração`)
+}
