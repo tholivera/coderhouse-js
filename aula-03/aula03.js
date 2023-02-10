9dfc2e73a83a9dce263fb4fb8083ff284d482f37
tholivera <emaildathaisoliveira.silva@gmail.com>
2023-02-09 22:07:57 -0300

    Alterando if para switch/case

--- aula-03/aula03.js
+++ aula-03/aula03.js
@@ -12,30 +12,33 @@ let frase = ["É em meio a dificuldade que se encontra a oportunidade",
 let tabuada = []
 
 do {
-    opcao = prompt("Escolha uma opção: "
+    opcao = parseInt(prompt("Escolha uma opção: "
         + "\n1 - Mostrar uma frase motivadora"
         + "\n2 - Mostrar o resultado de uma tabuada até o 10"
-        + "\n0 - Sair")
-
-    if (opcao == 1) {
-        for (let x = 1; x < 2; x++) {
-            alert(frase[Math.floor(Math.random() * frase.length)])
-        }
-        opcao = 0
-    } else if (opcao == 2) {
-        let numTabuada = parseInt(prompt("Digite o número da tabuada que você deseja"))
-        if (isNaN(numTabuada)) {
-            alert("Você não digitou um número")
-            opcao = 0
-        } else {
-            for (let x = 1; x < 11; x++) {
-                let addArray = [(`${numTabuada + "x" + x + "=" + numTabuada * x}`)]
-                tabuada.push(addArray)
+        + "\n0 - Sair"))
+    switch (opcao) {
+        case 1:
+            for (let x = 1; x < 2; x++) {
+                alert(frase[Math.floor(Math.random() * frase.length)])
             }
-            alert(tabuada.join('\n'))
-        }
-        opcao = 0
+            opcao = 0
+            break
+        case 2:
+            let numTabuada = parseInt(prompt("Digite o número da tabuada que você deseja"))
+            if (isNaN(numTabuada)) {
+                alert("Você não digitou um número")
+                opcao = 0
+            } else {
+                for (let x = 1; x < 11; x++) {
+                    let addArray = [(`${numTabuada + "x" + x + "=" + numTabuada * x}`)]
+                    tabuada.push(addArray)
+                }
+                alert(tabuada.join('\n'))
+                opcao = 0
+            }
+            break
     }
+
 } while (opcao != 0) {
     alert("Agradeçemos a sua participação")
 }
